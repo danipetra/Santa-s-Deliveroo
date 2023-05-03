799ab2fd8d6d082ed883131ec7caacf8d8fc1d79
Daniele Petracca <daniele.petracca.dp@gmail.com>
2023-05-03 12:32:08 +0200

    Prefab folder

--- Assets/Scripts/Santa.cs
+++ Assets/Scripts/Santa.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Santa : Agent
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
